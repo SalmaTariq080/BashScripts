2d608c9c9298545aafb83a07e3b20e62a7936254
SalmaTariq080 <45554496+SalmaTariq080@users.noreply.github.com>
2023-07-30 23:29:30 +0500

    Delete copy_files.sh

--- copy_files.sh
+++
@@ -1,5 +0,0 @@
-#cp -r Exome_Sequencing /nfs/mount-synology
-#cp -r wgs /nfs/mount-synology/wgs_analysis
-#cp -r dragen_chm13_index /nfs/mount-synology
-#cp -r Known_sites /nfs/mount-synology
-cp -r Reference /nfs/mount-synology
